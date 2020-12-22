d923afc91bfc185af207b8b43aed9fd806d1ed1b
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-22 01:40:04 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,17 @@
 class Ship
+  attr_reader :name, :type, :body
+
+  @@ships = []
+
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:type]
+    @body = args[:body]
+
+    @@ships << self
+  end
+
+  def self.all
+    @@ships
+  end
 end
